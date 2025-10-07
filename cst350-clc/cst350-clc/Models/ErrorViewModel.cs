0022986b2aea375ec824aa11cc49ca54ab8536b1
Caleb Overmyer <C.p.overmyer@outlook.com>
2025-10-07 02:08:40 -0700

    project init

---
+++ cst350-clc/cst350-clc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cst350_clc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
